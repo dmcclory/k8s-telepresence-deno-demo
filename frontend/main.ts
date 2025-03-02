1d89fcde361795a72645c10d48363325e7afaea8
Dan McClory <dmcclory@promptworks.com>
2025-03-02 13:42:53 -0500

    frontend service which selects random movie/id & calls the middle_layer

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,8 +1,30 @@
-export function add(a: number, b: number): number {
-  return a + b;
+// this combines some example code from:
+//
+// https://docs.deno.com/examples/environment_variables/
+// https://docs.deno.com/examples/http_server_routing/
+
+const PORT = Number(Deno.env.get("PORT") || "3006")
+
+const MIDDLE_HOST = Deno.env.get("MIDDLE_HOST") || "http://localhost:3005"
+
+const randomInt = () => {
+  return Math.floor(Math.random() * 10)
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+const randomMediaType = () => {
+  return randomInt() % 2 === 0 ? 'movies' : 'books'
 }
+
+const randomMediaId = () => {
+  return (randomInt() % 5) + 1
+}
+
+async function handler(req: Request): Promise<Response> {
+  const media_type = randomMediaType()
+  const media_id = randomMediaId()
+  console.log("we have???", {media_id, media_type})
+  const result = await fetch(`${MIDDLE_HOST}?media_type=${media_type}&media_id=${media_id}`)
+  return result
+}
+
+Deno.serve({ port: PORT}, handler);
